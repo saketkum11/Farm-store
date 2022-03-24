3a78b1e165d95df6fefd05521cb41835e0ef724a
saketkum11 <kumarsaket601@gmail.com>
2022-03-24 15:56:26 +0530

    added:authcontext

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import "./index.css";
 import App from "./App";
 import { makeServer } from "./server";
 import {BrowserRouter} from "react-router-dom"
+import { AuthProvider } from "./Component/Auth/Auth.js";
 // Call make Server
 makeServer();
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-    <App />
+    <AuthProvider>
+       <App />
+    </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
