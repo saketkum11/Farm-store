dc0c19f9dc8acbcb0d6d2bbf3593835c2ec0700c
saketkum11 <kumarsaket601@gmail.com>
2022-03-25 17:23:25 +0530

    added:signup auth

--- src/Context/Auth/Auth.js
+++ src/Context/Auth/Auth.js
@@ -5,6 +5,22 @@ const AuthContext = createContext();
 const useAuth = () => useContext(AuthContext);
 
 const AuthProvider = ({children})  => {
+
+   const signupHandler = async () => {
+      try {
+        const response = await axios.post(`/api/auth/signup`, {
+          firstName: "Adarsh",
+          lastName: "Balika",
+          email: "adarshbalika@neog.camp",
+          password: "adarshBalika",
+        });
+        // saving the encodedToken in the localStorage
+        localStorage.setItem("token", response.data.encodedToken);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
   
    return(<AuthContext.Provider value={{id:"saket",password:"djshdjhf"}}>{children}</AuthContext.Provider>)
 }
--- src/Pages/Auth/Signup.jsx
+++ src/Pages/Auth/Signup.jsx
@@ -1,6 +1,9 @@
-import React  from "react";
+import React from "react";
 import Navbar from "../../Component/Navbar/Navbar";
+
 function Signup() {
+
+const {email} = useAuth();
 
 return (
 <>
@@ -14,30 +17,34 @@ return (
                 <div className=" bg-black-0 flex flex-column  m-t-8 wt-100 ">
                     <label for="" className="bg-white-0 ">First Name
                         <input 
-                        
-                        type="text" 
-                        placeholder="Anish"
-                        className=" rounded-xs pd-3 wt-100 " />
+                         type="text" 
+                         placeholder="Anish"
+                         className=" rounded-xs pd-3 wt-100 " />
                     </label>
                 </div>
                 <div className=" bg-black-0 flex flex-column  m-t-8 wt-100 ">
                     <label for="" className="bg-white-0 ">Last Name
-                        <input  type="text" placeholder="Kumar" className=" rounded-xs pd-3 wt-100 " />
+                        <input 
+                        type="text" 
+                        placeholder="Kumar" 
+                        className=" rounded-xs pd-3 wt-100 " />
                     </label>
                 </div>
                 <div className=" bg-black-0 flex flex-column  m-t-8 wt-100 ">
                     <label for="" className="bg-white-0 ">Email address
-                        <input  type="email" placeholder="saketkumar@gmail.com"
-                            className=" rounded-xs pd-3 wt-100 " /></label>
+                        <input 
+                        type="email" 
+                        placeholder="saketkumar@gmail.com"
+                        className=" rounded-xs pd-3 wt-100 " /></label>
                 </div>
                 <div className="bg-black-0 flex  flex-column  m-y-4 wt-100">
                     <label for="" className="bg-white-0">Password
-                        <input  type="password" placeholder="***************************"
+                        <input type="password" placeholder="***************************"
                             className="rounded-xs pd-3  wt-100" /></label>
                 </div>
                 <div className="flex  text-s m-y-4 justify-btw wt-100 flex-wrap">
                     <div className="flex items-center   ">
-                        <input  type="checkbox" className="m-x-2" name="" id="" />
+                        <input type="checkbox" className="m-x-2" name="" id="" />
                         <label for="">I accept all Term & Conditions.</label>
                     </div>
 
@@ -45,6 +52,7 @@ return (
                 </div>
                 <div className="wt-100">
                     <button
+                       onClick={()=>dispatch({type:"SIGN_UP",payload:""})}
                         className="cursor bg-red-7 text-color-0 text-xm outline-none border-none pd-x-11 pd-y-3 rounded-xs wt-100">Sign
                         Up</button>
                 </div>
