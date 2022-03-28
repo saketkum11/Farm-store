d655d78cc25dbe1998c93853b7056faf71d26f6b
saketkum11 <kumarsaket601@gmail.com>
2022-03-28 11:01:52 +0530

    modified:self closing tags

--- src/App.js
+++ src/App.js
@@ -13,13 +13,13 @@ function App() {
   return (
     <div className="App">
      <Routes>
-       <Route path="/" element={<Home></Home>}></Route>
-       <Route path="/product" element={<Product></Product>}></Route>
-       <Route path="/wishlist" element={<Wishlist></Wishlist>}></Route>
-       <Route path="/cart" element={<Cart></Cart>}></Route>
-       <Route path="/login" element={<Login></Login>}></Route>
-       <Route path="/signup" element={<Signup></Signup>}></Route>
-       <Route path="/forgot" element={<Forgot></Forgot>}></Route>
+       <Route path="/" element={<Home/>}></Route>
+       <Route path="/product" element={<Product/>}></Route>
+       <Route path="/wishlist" element={<Wishlist/>}></Route>
+       <Route path="/cart" element={<Cart/>}></Route>
+       <Route path="/login" element={<Login/>}></Route>
+       <Route path="/signup" element={<Signup/>}></Route>
+       <Route path="/forgot" element={<Forgot/>}></Route>
        <Route path="/mock" element={<Mockman/>}></Route>
      </Routes>
     </div>
