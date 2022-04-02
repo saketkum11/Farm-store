1eed89f059ece495c934b84e0e048c7d006aa301
saketkum11 <kumarsaket601@gmail.com>
2022-04-02 17:41:08 +0530

    added:post request

--- src/Context/Wishlist/Wishlist-Context.js
+++ src/Context/Wishlist/Wishlist-Context.js
@@ -1,9 +1,31 @@
-import React ,{createContext,useContext} from "react";
+import axios from "axios";
+import React  from "react";
+import { useEffect,createContext,useContext} from "react";
 
 const wishlistContext = createContext();
 const useWishlist = () => useContext(wishlistContext);
 
 const WishlistProvider = ({children}) => {
+   
+    useEffect(()=>{
+
+       const getWishlist = async () => {
+
+        try {
+
+            const response = await axios.post("/api/user/wishlist");
+            console.log("response",response.data.wishlist)
+            
+        } catch (error) {
+            console.log(error)
+        }
+
+       }
+    getWishlist()
+        
+    }
+    ,[])
+
     return(<wishlistContext.Provider value={{items:8}}>{children}</wishlistContext.Provider>)
 }
 export {WishlistProvider,useWishlist}
